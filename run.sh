3a09b73171c056e2deda9d8ac3ac03c68800f8b5
dk12651 <58054076+dk12651@users.noreply.github.com>
2025-03-15 15:55:31 -0400

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-{
-    "build_from": {
-      "aarch64": "homeassistant/aarch64-base-debian:buster",
-      "amd64": "homeassistant/amd64-base-debian:buster"
-    },
-    "squash": false,
-    "args": {
-    }
-  }
