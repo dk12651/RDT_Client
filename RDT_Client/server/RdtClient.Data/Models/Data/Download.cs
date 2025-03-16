8fb5c323918814f273ad33b67e5e131d16f0c28b
dk12651 <58054076+dk12651@users.noreply.github.com>
2025-03-16 12:00:09 -0400

    Add files via upload

---
+++ RDT_Client/server/RdtClient.Data/Models/Data/Download.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RdtClient.Data.Models.Data;
+
+public class Download
+{
+    [Key]
+    public Guid DownloadId { get; set; }
+
+    public Guid TorrentId { get; set; }
+
+    [ForeignKey("TorrentId")]
+    public Torrent? Torrent { get; set; }
+
+    public String Path { get; set; } = null!;
+    public String? Link { get; set; }
+
+    public DateTimeOffset Added { get; set; }
+    public DateTimeOffset? DownloadQueued { get; set; }
+    public DateTimeOffset? DownloadStarted { get; set; }
+    public DateTimeOffset? DownloadFinished { get; set; }
+    public DateTimeOffset? UnpackingQueued { get; set; }
+    public DateTimeOffset? UnpackingStarted { get; set; }
+    public DateTimeOffset? UnpackingFinished { get; set; }
+    public DateTimeOffset? Completed { get; set; }
+
+    public Int32 RetryCount { get; set; }
+        
+    public String? Error { get; set; }
+
+    public String? RemoteId { get; set; }
+
+    public String? FileName { get; set; }
+
+    [NotMapped]
+    public Int64 BytesTotal { get; set; }
+
+    [NotMapped]
+    public Int64 BytesDone { get; set; }
+
+    [NotMapped]
+    public Int64 Speed { get; set; }
+}
